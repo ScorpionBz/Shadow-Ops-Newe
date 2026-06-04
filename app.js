c9915bf6ab2c1440980a7e70b3731abc59fab522
ScorpionBz <scorpbz.oficial@gmail.com>
2026-06-04 08:18:35 -0600

    Initialize Firebase in app.js
    Added Firebase initialization code to app.js.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // SHADOW OPS NEXUS 1.0
-
+```js import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js"; import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js"; import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js"; const firebaseConfig = { apiKey: "AIzaSyCBE0hhMLUQR91JokZFxSm1119s5tmQ0k4", authDomain: "shadow-ops-nexus.firebaseapp.com", projectId: "shadow-ops-nexus", storageBucket: "shadow-ops-nexus.firebasestorage.app", messagingSenderId: "422758972704", appId: "1:422758972704:web:a57c4ea46a20d6d604eb45" }; const app = initializeApp(firebaseConfig); const db = getDatabase(app); const auth = getAuth(app); ```
 const $ = id => document.getElementById(id);
 
 // Navegación
